016abd2e211a2b1038ee50a7958b69d82eb8f9b0
Kelechi <kelechiuma@gmail.om>
2021-12-05 21:10:48 +0100

    Sliders added with styles

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: 'https://i.ibb.co/cXFnLLV/3.png',
+    title: 'SUMMER SALE',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: 'f5fafc',
+  },
+  {
+    id: 2,
+    img: 'https://i.ibb.co/DG69bQ4/2.png',
+    title: 'DISCOUNT SALE',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: 'fcf1ed',
+  },
+  {
+    id: 2,
+    img: 'https://i.ibb.co/DG69bQ4/2.png',
+    title: 'AMAZING DEAL',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: 'fcf1ed',
+  },
+];
